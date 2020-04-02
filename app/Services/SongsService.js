c1320228e8dfa2823508e5fde0a8e9962e1b5dc1
Preston Hess <hesspreston@outlook.com>
2020-04-02 16:43:54 -0700

    filter search query for only music

--- app/Services/SongsService.js
+++ app/Services/SongsService.js
@@ -29,7 +29,7 @@ class SongsService {
    */
   getMusicByQuery(query) {
     //NOTE You will not need to change this method
-    let url = "https://itunes.apple.com/search?callback=?&term=" + query;
+      let url = "https://itunes.apple.com/search?callback=?&term=" + query + "&entity=musicTrack";
     // @ts-ignore
     $.getJSON(url)
     .then(res => {
